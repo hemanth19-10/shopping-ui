dd9c7262603b767e37fb20fd69c5e055e95f1bc2
unknown <you@example.com>
2026-04-02 15:08:26 +0530

    your message

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 
 import React from 'react'
 import './App.css'
-import Mainpage from '../shopping/pages/Mainpage'
+import Mainpage from './shopping/pages/mainpage'
+
 const App = () => {
   return (
     <div className='background'>
