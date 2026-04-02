626440d063001b71e266c98fd90e13f219e73e4f
unknown <you@example.com>
2026-04-02 14:57:09 +0530

    your message

--- src/shopping/pages/Mainpage.jsx
+++ src/shopping/pages/Mainpage.jsx
@@ -1,24 +1,24 @@
-
-
-import React , {useState} from 'react'
+import React, { useState } from 'react'
 import Footer from '../componets/footer'
 import Collection from '../componets/collection'
 import Banner from '../componets/banner'
 import Header from '../componets/header'
-import {Gents , Ladies} from '../data'
+import { Gents, Ladies } from '../data'
 import WomenCollection from '../componets/womenCollection'
- 
+
 const Mainpage = () => {
-    const [gentsFashion ,setGentsFashion ]= useState(Gents)
-    const [ladiesFashion ,setladiesFashion ]= useState(Ladies)
-    console.log(Gents)
+  const [gentsFashion, setGentsFashion] = useState(Gents)
+  const [ladiesFashion, setladiesFashion] = useState(Ladies)
+
+  console.log(Gents)
+
   return (
     <div>
-        <Header/>
-        <Banner/>
-        <Collection gentsFashion={gentsFashion}/>
-        <WomenCollection ladiesFashion={ladiesFashion}/>
-        <Footer/>
+      <Header />
+      <Banner />
+      <Collection gentsFashion={gentsFashion} />
+      <WomenCollection ladiesFashion={ladiesFashion} />
+      <Footer />
     </div>
   )
 }
